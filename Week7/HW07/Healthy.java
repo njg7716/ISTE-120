e2ef3a0964df0b85a8c4396caddcf896b54fc24e
njg7716 <njg7716@rit.edu>
2020-03-05 12:28:11 -0500

    HW7 done

--- Week7/HW07/Healthy.java
+++ Week7/HW07/Healthy.java
@@ -1,3 +1,8 @@
+/*
+Nicholas Graca
+ISTE-120
+HW7
+*/
 public class Healthy{
    private String name;
    private String gender;
@@ -14,51 +19,66 @@ public class Healthy{
       age = a;
       activity = act;
    }
+   //Returns name of person
    public String getName(){
       return name;
    }
+   //returns gender of person
    public String getGender(){
       return gender;
    }
+   //returns weight of person
    public double getWeight(){
       return weight;
    }
+   //returns height of person
    public double getHeight(){
       return height;
    }
+   //returns weight of person
    public int getAge(){
       return age;
    }
+   //returns activity level of person
    public int getActivity(){
       return activity;
    }
+   //Sets name to the string given
    public void setName(String n){
       name = n;
    }
+   //sets gender to given value
    public void setGender(String g){
       gender = g;
    }
+   //sets weight to given number
    public void setWeight(double w){
       weight = w;
    }
+   //set height to given number
    public void setHeight(double h){
       height = h;
    }
+   //sets age to given int
    public void setAge(int a){
       age = a;
    }
+   //set activity level to given int
    public void setActivity(int a){
       activity = a;
    }
+   //returns the persons BMI
    public double calcBMI(){
       return (weight/(height*height))*703;
    }
+   //returns the persons BMR
    public double calcBMR(){
       if (gender.equals("M")){
-         return 66 + (13.7 * weight * .454) + (5 * height * 2.54) - (6.8 * age);
+         return 66 + (13.7 * imperialWeight()) + (5 * height * 2.54) - (6.8 * age);
       }
-      return 655 + (9.6 * weight * .454) + (1.8 * height * 2.54) - (4.7 * age);
+      return 655 + (9.6 * weight * .454) + (1.8 * imperialHeight()) - (4.7 * age);
    }
+   //Uses the BMR method and activity value and returns TDEE value
    public double calcTDEE(){
       double mult = 0;
       if (getActivity() == 1){
@@ -77,5 +97,30 @@ public class Healthy{
          mult = 1.9;
       }
       return calcBMR() * mult;
+   }
+   //returns the weight in kilograms
+   public double imperialWeight(){
+      return weight * .454;
+   }
+   //returns the height in centimeters
+   public double imperialHeight(){
+      return height * 2.54;
+   }
+   //Determines persons weight classification based on BMI
+   public String weightClass(){
+      String clas = "";
+      if (calcBMI() < 18.5){
+         clas = "Underweight";
+      }
+      else if (calcBMI() < 25){
+         clas = "Normal weight";
+      }
+      else if (calcBMI() < 30){
+         clas = "Overweight";
+      }
+      else{
+         clas = "Obese";
+      }
+      return clas;
    }
 }
--- Week7/HW07/HowHealthy.java
+++ Week7/HW07/HowHealthy.java
@@ -1,13 +1,20 @@
+/*
+Nicholas Graca
+ISTE-120
+HW7
+*/
 import java.util.Scanner;
 public class HowHealthy{
    public static void main(String[] args){
       Scanner reader = new Scanner(System.in);
+      //Ask user for name and validate
       System.out.print("Person's Name: ");
       String name = reader.nextLine();
       if (name.length() < 1){
          System.out.println("Name must be at least a character.");
          System.exit(0);
       }
+      //Ask user for gender and validate
       System.out.print(name + ", are you male or female (M/F): ");
       String gender = reader.nextLine().toUpperCase();
       if (gender.length() > 1){
@@ -18,24 +25,28 @@ public class HowHealthy{
          System.out.println("Gender must be M or F.");
          System.exit(0);
       }
+      //ask user for weight and validate
       System.out.print(name + "'s weight (pounds): ");
       int weight = reader.nextInt();
       if (weight < 100){
-         System.out.println("Weight at least 100 pounds.");
+         System.out.println("Invalid weight - must be at least 100 pounds.");
          System.exit(0);
       }
+      //ask user for height and validate
       System.out.print(name + "'s height (inches): ");
       int height = reader.nextInt();
       if (height < 60 || height > 84){
-         System.out.println("Height must be between 60 and 84 inches.");
+         System.out.println("Invalid height - must be 60..84, inclusively.");
          System.exit(0);
       }
+      //ask user for age and validate
       System.out.print(name + "'s age (years): ");
       int age = reader.nextInt();
       if (age < 18 ){
-         System.out.println("Must be at least 18 years old.");
+         System.out.println("Invalid age - must be at least 18.");
          System.exit(0);
       }
+      //Ask user for activity level and validate
       System.out.println();
       System.out.println("\nActivity Level: Use these categories:");
       System.out.println("\t1 - Sedentary (little or no exercise, desk job)");
@@ -49,34 +60,24 @@ public class HowHealthy{
          System.out.println("Must be a value between 1 and 5 (inclusive).");
          System.exit(0);
       }
+      //Using info make a new person and print their information
       Healthy person = new Healthy(name, gender, weight, height, age, active);
       System.out.println(person.getName() + "'s information");
       System.out.println("Weight: " + person.getWeight() + " pounds");
       System.out.println("Height: " + person.getHeight() + " inches");
       System.out.println("Age: " + person.getAge() + " years");
+      //Check gender and prin the appopriate sentence
       if (person.getGender().equals("F")){
          System.out.println("These are for a female.");      
       }
       if (person.getGender().equals("M")){
          System.out.println("These are for a male.");      
       }
+      //Print users BMI, BMR, TDEE and weight classification
       System.out.printf("\nBMR is %.2f\n", person.calcBMR());
       System.out.printf("BMI is %.2f\n", person.calcBMI());
       System.out.printf("TDEE is %.2f\n", person.calcTDEE());
-      String clas = "";
-      if (person.calcBMI() < 18.5){
-         clas = "Underweight";
-      }
-      else if (person.calcBMI() < 25){
-         clas = "Normal weight";
-      }
-      else if (person.calcBMI() < 30){
-         clas = "Overweight";
-      }
-      else{
-         clas = "Obese";
-      }
-      System.out.println("Your BMI classifies you as " + clas);
+      System.out.println("Your BMI classifies you as " + person.weightClass());
    }
 
 }
